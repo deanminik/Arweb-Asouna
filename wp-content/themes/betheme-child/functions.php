740a9b60cf4f88891292c29e3be22755622c7b26
Dean <deaneryfb@gmail.com>
2022-05-23 10:51:08 -0600

    Start project with essential scss folders

---
+++ wp-content/themes/betheme-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+//IMPORTANT -> To find the main css file from the father theme, not always is style.css
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles', 11);
+function theme_enqueue_styles()
+{
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/be.min.css');
+    wp_enqueue_style(
+        'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
